829c1fe9ba0c223f09c4f233b6b49f77d7404f5c
ozymandias <lusca@ozy>
2025-02-11 14:16:00 -0300

    comentada algumas seções

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import './App.css';
-import Header from './components/Header';  // Importando o Header
-import HeroSection from './components/HeroSection';  // Importando o HeroSection
-import AboutSection from './components/AboutSection';  // Importando o AboutSection
-import ServicesSection from './components/ServicesSection';  // Importando o ServicesSection
-import InfoSection from './components/InfoSection';  // Importando o InfoSection
+//import Header from './components/Header';  // Importando o Header
+//import HeroSection from './components/HeroSection';  // Importando o HeroSection
+//import AboutSection from './components/AboutSection';  // Importando o AboutSection
+//import ServicesSection from './components/ServicesSection';  // Importando o ServicesSection
+//import InfoSection from './components/InfoSection';  // Importando o InfoSection
 import ExperienceSection from './components/ExperienceSection';  // Importando a nova ExperienceSection
-import Pricing from './components/Pricing';
-import TeamSection from './components/TeamSection';
-import CaseStudies from './components/CaseStudies';
-import Testimonials from './components/Testimonials';
-import YouReadySection from './components/YouReadySection';
-import FAQ from './components/FAQ';
+//import Pricing from './components/Pricing';
+//import TeamSection from './components/TeamSection';
+//import CaseStudies from './components/CaseStudies';
+//import Testimonials from './components/Testimonials';
+//import YouReadySection from './components/YouReadySection';
+//import FAQ from './components/FAQ';
 import ContactSection from './components/ContactSection';
 
 
 function App() {
   return (
     <div className="App">
-      {/* Cabeçalho da página */}
+      {/* Cabeçalho da página 
       <Header />
 
-      {/* Seção Hero */}
+      Seção Hero 
       <HeroSection />
 
-      {/* Seção About */}
+      Seção About 
       <AboutSection />
 
-      {/* Seção de Serviços */}
+      Seção de Serviços 
       <ServicesSection />
 
-      {/* Seção de Info */}
-      <InfoSection />
+      Seção de Info 
+      <InfoSection />*/}
 
-      {/* Nova Seção */}
       <ExperienceSection />
 
-      {/* Nova Seção */}
+      {/* 
       <Pricing/>
 
       <TeamSection/>
@@ -46,21 +45,16 @@ function App() {
 
       <YouReadySection/>
 
-      <FAQ/>
+      <FAQ/>*/}
 
       <ContactSection/>
 
 
-     
-
-      
-
-      {/* Conteúdo da página */}
-      <div className="content">
+      {/* <div className="content">
         <p>Bem-vindo ao nosso site! Estamos felizes em tê-lo aqui.</p>
       </div>
 
-      {/* Outras seções podem ser adicionadas aqui */}
+      Outras seções podem ser adicionadas aqui */}
     </div>
   );
 }
